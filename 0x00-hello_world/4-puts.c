0769af986f612f1d96724052cbd5bd172c3c4f76
Seid <letter_seid@protonmail.com>
2022-04-07 16:35:58 -0700

    added comment on task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - The Entry point to the program
+ *
+ * Return: end value 0 
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
