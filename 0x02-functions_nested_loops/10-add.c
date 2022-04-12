c2f5f6723892cdaba5a9a47c28f94e495ddf4c1b
Seid <letter_seid@protonmail.com>
2022-04-12 16:42:32 -0700

    Added Task10: a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @num1: argument1
+ * @num2: argument2
+ * Return: the sum of the two arguments
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
