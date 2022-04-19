e26d495a9220e482b2eaed68f52fe79a88e1d557
Seid <letter_seid@protonmail.com>
2022-04-18 18:31:09 -0700

    correction

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,10 +9,17 @@
  */
 int main(void)
 {
-	int num;
+	int sum;
+	char c;
 
-	srand(time(0));
-	num = rand();
-	printf("%i\n", num);
+	srand(time(NULL));
+	
+	while (sum <= 2645)
+	{
+		c = rand() % 128;
+		sum += c;
+		putchar(c);
+	}
+	putchar(2772 - sum);
 	return (0);
 }
