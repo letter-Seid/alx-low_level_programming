7edc1a86cd2bd5797c1d91f28ff986ed86eff787
Seid <letter_seid@protonmail.com>
2022-04-20 16:38:46 -0700

    corrected task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,64 +1,6 @@
 #include "main.h"
-
 /**
- * _atoi -  adds two numbers
- * @n1: first number
- * @n2: second number
- * @r: result
- * @size_r: result length
- * Return: pointer to the sum or 0
- */
-int _atoi(char *s)
-{
-	int sign = 1, resp = 0, firstNum;
-
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
-		}
-	}
-
-	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-
-	return (sign * resp);
-}
-
-/**
- * int_to_string -  converts integer to string
- * @n: int number
- *
- * Return: number characters
- */
-void int_to_string(int n)
-{
-	int divisor = 1, i, resp;
-
-
-	for (i = 0; n / divisor > 9; i++)
-	{
-		divisor *= 10;
-	}
-
-	char str[i];
-
-	for (int cmpt = 0; divisor >= 10; divisor /= 10, cmpt++)
-	{
-		resp = n / divisor;
-		str[cmpt] = '0' + resp;
-		n = n - resp * divisor;
-	}
-	str[i] = ('0' + n);
-
-}
-
-/**
- * *infinite_add -  adds two numbers
+ * infinite_add -  adds two numbers
  * @n1: first number
  * @n2: second number
  * @r: result
@@ -67,10 +9,44 @@ void int_to_string(int n)
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int sum, a, b;
-	a = _atoi(n1);
-	b = _atoi(n2);
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	sum = a + b;
-
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
+		return (0);
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
+	{
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
+	}
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
