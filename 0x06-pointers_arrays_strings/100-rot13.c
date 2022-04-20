d49d100bd54cb4c0c78e8169110928c45ca4e497
Seid <letter_seid@protonmail.com>
2022-04-20 15:56:27 -0700

    Added Task8: rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * *rot13 - function that encodes a string using rot13
+ * @s: pointer to the variable s
+ * Return: return pointer to the variable s
+ */
+char *rot13(char *s)
+{
+
+	int i;
+	char *p = s;
+
+	char *original = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (*s != '\0')
+	{
+		i = 0;
+		while (i < 53)
+		{
+			if (*s == original[i])
+			{
+				*s = code[i];
+				break;
+			}
+			i++;
+		}
+		s++;
+	}
+
+	return (p);
+
+}
