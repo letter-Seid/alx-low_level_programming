ca89f1e4d9044d88d309d4719f89fe13cd7b2033
Seid <letter_seid@protonmail.com>
2022-04-20 15:56:21 -0700

    Added Task0: strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest : destination string
+ * @src : the string to appended
+ * Return: merged chars
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
