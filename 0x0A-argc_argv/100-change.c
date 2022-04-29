ace206ae3101a8c9b566cad32b56ca1fe4935379
Seid <letter_seid@protonmail.com>
2022-04-29 08:47:52 -0700

    fun is less than 40 line now

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
-
 /**
  * main - Find the smallest number of coins to make an amount of change
  * @argc: Arg count
@@ -12,7 +11,6 @@
 int main(int argc, char *argv[])
 {
 	int cents, coins = 0;
-
 	if (argc == 2)
 	{
 		cents = atoi(*(argv + 1));
